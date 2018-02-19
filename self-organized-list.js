2c5023a583831a1dd2796ff62cfee887c4927af8
arnoldmir <36645674+arnoldmir@users.noreply.github.com>
2018-02-20 00:27:14 +0300

    Add files via upload

---
+++ self-organized-list.js
@@ -0,0 +1,72 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class SelfOrganizedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    insert(data) {
+this.length++;
+    }
+
+    size() {
+return this.length;
+    }
+
+    at(index) {
+if (index==0) return this.head.data;
+if (index<this.length && index>0)
+{
+  var node=new Node();
+  node=this.head.next;
+  for (var i=1; i <index; i++)
+  node=node.next;
+  return node.data;
+}
+else return null;
+    }
+
+    findNode(data) {
+for(var i=0; i<this.length; i++)
+{
+  var node=this.atNode(i);
+  if(node.data==data)
+  {
+    return node;
+  }
+}
+    }
+
+    toArray() {
+var arr =[];
+for(var i=0; i<this.length;i++)
+arr.push(this.at(i));
+return arr;
+    }
+
+    removeAt(index) {
+this.length--;
+    }
+
+    moveToFront(node) {
+
+    }
+
+    reorganize(data) {
+
+    }
+
+}
+
+module.exports = {
+    SelfOrganizedList,
+    Node
+};
